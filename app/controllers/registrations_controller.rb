12fffbb6c546849db910d0d1a80580e36164d3be
nguyenvantrung2015 <nguyenvantrung2015@gmail.com>
2022-06-25 12:07:57 +0900

    create the login and the logout function

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,10 +1,10 @@
 class RegistrationsController < ApplicationController
   def new ;end
-  def new2 ;end
   def create
     user = User.new(user_params)
     if user.save
-      flash[:success] = "登録に成功しました。"
+      sign_in(:user, user)
+      flash[:success] = "登録に成功してログインしました。"
       redirect_to root_path
     else
       flash[:danger] = user.errors.full_messages
